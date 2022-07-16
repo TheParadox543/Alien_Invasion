ae101d184f1acf0f11d55ac2adfe599785a9fd4f
TheParadox543 <samuel.alex.koshy@gmail.com>
2022-07-16 23:24:50 +0530

    Draw ship to screen
    Init now has an instance of a ship, and the screen fills it every time.
    ship.py had to be modified since circular import was preventing it from running.

--- ship.py
+++ ship.py
@@ -1,14 +1,12 @@
 import pygame
 
-from alien_invasion import AlienInvasion
-
 class Ship:
     """A class to manage the ship."""
 
-    def __init__(self, ai_game:AlienInvasion):
+    def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
-        self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
+        self.screen:pygame.Surface = ai_game.screen
+        self.screen_rect:pygame.Rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
