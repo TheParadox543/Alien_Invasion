a356db855a3863312827229f26fd230a2e5c8ef0
TheParadox543 <samuel.alex.koshy@gmail.com>
2022-07-17 11:54:50 +0530

    limit ship's range
    The ship will now stay within the window, whereas before it would move out of the screen if the keys were held on for too long.

--- ship.py
+++ ship.py
@@ -27,12 +27,15 @@ class Ship:
 
     def update(self):
         """Update the ship's position based on the movement flag."""
-        if self.moving_right: 
+        # Update the ship's x value.
+        if self.moving_right \
+                and self.rect.right < self.screen_rect.right: 
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left \
+                and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
-        # Update rect object from self.x
+        # Update rect object from self.x.
         self.rect.x = self.x
 
     def blitme(self):
