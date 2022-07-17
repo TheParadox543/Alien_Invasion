83138795ae682c35653af778147210fe81713e30
TheParadox543 <samuel.alex.koshy@gmail.com>
2022-07-17 17:31:45 +0530

    create row of aliens
    We create a row of aliens to space it on the screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,9 +93,20 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Create a fleet of aliens."""
-        # Make an alien
+        # Create an alien and find number of aliens in a row.
+        # Spacing between two aliens is one alien's space.
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Create the first row of aliens.
+        for alien_number in range(number_aliens_x):
+            # Create an alien and place it in the row.
+            alien  = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
