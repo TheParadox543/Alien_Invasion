1547fc3caf69bdb736a666bba580dd5c9a550c21
TheParadox543 <samuel.alex.koshy@gmail.com>
2022-07-17 20:25:10 +0530

    shooting aliens
    Check for collisions between bullets and aliens and remove them if true.

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,6 +93,11 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        # Check for any bullets that have hit aliens.
+        #   If so, get rid of the bullet and alien.
+        collisions = pygame.sprite.groupcollide(self.bullets,
+                self.aliens, True, True)
+
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
         self._check_fleet_edges()
